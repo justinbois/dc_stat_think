6827f276c960bd980e5c2cac38ace15bde1bdd08
Justin Bois <bois@caltech.edu>
2017-07-24 16:52:55 -0700

    Updated to version 0.1.2.

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ test_requirements = [
 
 setup(
     name='dc_stat_think',
-    version='0.1.1',
+    version='0.1.2',
     description="Utility functions used in the DataCamp Statistical Thinking courses.",
     long_description=readme + '\n\n' + history,
     author="Justin Bois",
