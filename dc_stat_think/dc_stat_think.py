37afc5b07cd20214ba5b00ed540574da09005609
Justin Bois <bois@caltech.edu>
2017-07-25 09:20:53 -0700

    Updated ECDF to include formal kwarg.

--- dc_stat_think/dc_stat_think.py
+++ dc_stat_think/dc_stat_think.py
@@ -4,9 +4,111 @@
 
 import numpy as np
 
+def ecdf(data, formal=False, buff=0.1, min_x=None, max_x=None):
+    """
+    Generate `x` and `y` values for plotting an ECDF.
 
-def ecdf(data):
+    Parameters
+    ----------
+    data : array_like
+        Array of data to be plotted as an ECDF.
+    formal : bool, default False
+        If True, generate `x` and `y` values for formal ECDF.
+        Otherwise, generate `x` and `y` values for "dot" style ECDF.
+    buff : float, default 0.1
+        How long the tails at y = 0 and y = 1 should extend as a
+        fraction of the total range of the data. Ignored if
+        `formal` is False.
+    min_x : float, default None
+        Minimum value of `x` to include on plot. Overrides `buff`.
+        Ignored if `formal` is False.
+    max_x : float, default None
+        Maximum value of `x` to include on plot. Overrides `buff`.
+        Ignored if `formal` is False.
+
+    Returns
+    -------
+    x : array
+        `x` values for plotting
+    y : array
+        `y` values for plotting
+    """
+
+    if formal:
+        return _ecdf_formal(data, buff=buff, min_x=min_x, max_x=max_x)
+    else:
+        return _ecdf_dots(data)
+
+
+def _ecdf_dots(data):
+    """
+    Compute `x` and `y` values for plotting an ECDF.
+
+    Parameters
+    ----------
+    data : array_like
+        Array of data to be plotted as an ECDF.
+
+    Returns
+    -------
+    x : array
+        `x` values for plotting
+    y : array
+        `y` values for plotting
+    """
     return np.sort(data), np.arange(1, len(data)+1) / len(data)
+
+
+def _ecdf_formal(data, buff=0.1, min_x=None, max_x=None):
+    """
+    Generate `x` and `y` values for plotting a formal ECDF.
+
+    Parameters
+    ----------
+    data : array_like
+        Array of data to be plotted as an ECDF.
+    buff : float, default 0.1
+        How long the tails at y = 0 and y = 1 should extend as a fraction
+        of the total range of the data.
+    min_x : float, default None
+        Minimum value of `x` to include on plot. Overrides `buff`.
+    max_x : float, default None
+        Maximum value of `x` to include on plot. Overrides `buff`.
+
+    Returns
+    -------
+    x : array
+        `x` values for plotting
+    y : array
+        `y` values for plotting
+    """
+    # Get x and y values for data points
+    x, y = _ecdf_dots(data)
+
+    # Set defaults for min and max tails
+    if min_x is None:
+        min_x = x[0] - (x[-1] - x[0])*buff
+    if max_x is None:
+        max_x = x[-1] + (x[-1] - x[0])*buff
+
+    # Set up output arrays
+    x_formal = np.empty(2*(len(x) + 1))
+    y_formal = np.empty(2*(len(x) + 1))
+
+    # y-values for steps
+    y_formal[:2] = 0
+    y_formal[2::2] = y
+    y_formal[3::2] = y
+
+    # x- values for steps
+    x_formal[0] = min_x
+    x_formal[1] = x[0]
+    x_formal[2::2] = x
+    x_formal[3:-1:2] = x[1:]
+    x_formal[-1] = max_x
+
+    return x_formal, y_formal
+
 
 def eccdf(data):
     """Generate x and y values for plotting an ECCDF."""
