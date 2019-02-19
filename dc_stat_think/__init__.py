78024f751caacdd7d5b04557f26d1c4d094ee801
justinbois <bois@caltech.edu>
2019-02-19 11:06:20 -0800

    Version 1.0.1. Minor bug fix to b_value function.

--- dc_stat_think/__init__.py
+++ dc_stat_think/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Justin Bois"""
 __email__ = 'bois@caltech.edu'
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 
 
 from .dc_stat_think import *
--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ test_requirements = [
 
 setup(
     name='dc_stat_think',
-    version='1.0.0',
+    version='1.0.1',
     description="Utility functions used in the DataCamp Statistical Thinking courses.",
     long_description=readme + '\n\n' + history,
     author="Justin Bois",
@@ -49,6 +49,7 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
     test_suite='tests',
     tests_require=test_requirements,
