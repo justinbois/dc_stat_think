58a8cb6a36fe3ff71e099f835c3e17a75bf8f23c
justinbois <bois@caltech.edu>
2019-02-20 09:32:56 -0800

    Release number 1.0.2.

--- dc_stat_think/__init__.py
+++ dc_stat_think/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Justin Bois"""
 __email__ = 'bois@caltech.edu'
-__version__ = '1.0.1'
+__version__ = '1.0.2'
 
 
 from .dc_stat_think import *
--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ test_requirements = [
 
 setup(
     name='dc_stat_think',
-    version='1.0.1',
+    version='1.0.2',
     description="Utility functions used in the DataCamp Statistical Thinking courses.",
     long_description=readme + '\n\n' + history,
     author="Justin Bois",
